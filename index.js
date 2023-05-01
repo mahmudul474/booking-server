9bdfb13dbb043eb3b6892704fba51436b74f742b
mahmudul474 <mdmahmudulla474@gmail.com>
2023-05-01 21:57:16 +0100

    home push

--- index.js
+++ index.js
@@ -21,21 +21,19 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = "mongodb+srv://bookingtaxi:sI4ItdmHCis9y0il@474.79d3jxt.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+ 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
 });
+
+
+
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    
 
     
     const carcollection=client.db("taxi-booking").collection("cars")
