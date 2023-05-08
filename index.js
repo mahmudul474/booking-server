393535513a59fbbcb25d7047b7aaac85fbe47f5b
mahmudul474 <mdmahmudulla474@gmail.com>
2023-05-08 09:21:01 +0600

    end morning

--- index.js
+++ index.js
@@ -1,89 +1,70 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
-const  express=require ("express")
-const cors=require("cors")
-const app=express()
-const port=process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("booking app server");
+});
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+const uri =
+  "mongodb+srv://bookingtaxi:sI4ItdmHCis9y0il@474.79d3jxt.mongodb.net/?retryWrites=true&w=majority";
 
-app.use(cors())
-app.use(express.json())
-
-  app.get('/',(req,res)=>{
-    res.send('booking app server')
-  })
-
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const uri = "mongodb+srv://bookingtaxi:sI4ItdmHCis9y0il@474.79d3jxt.mongodb.net/?retryWrites=true&w=majority";
-
- 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1
+  serverApi: ServerApiVersion.v1,
 });
-
-
-
 
 async function run() {
   try {
-    
+    const carcollection = client.db("taxi-booking").collection("cars");
+    const ordercollection = client.db("taxi-booking").collection("orders");
 
-    
-    const carcollection=client.db("taxi-booking").collection("cars")
-    const ordercollection=client.db("taxi-booking").collection("orders")
+    ///save car
 
+    app.post("/car", async (req, res) => {
+      const car = req.body;
+      const result = await carcollection.insertOne(car);
+      res.send(result);
+    });
 
-
-    ///save car 
-
-    app.post("/car", async(req, res) => {
-      const car=req.body
-       const result=await carcollection.insertOne(car)
+    app.get("/car", async (req, res) => {
+      const result = await carcollection.find({}).toArray();
+      res.send(result);
+    });
+ 
+    //get by id 
+  
+    app.get("/car/:id", async(req,res)=>{
+      const id=req.params.id
+      const query= {_id:ObjectId(id)}
+       const  result =await carcollection.findOne(query)
        res.send(result)
     })
 
 
-  app.get("/car",async(req, res) => {
-    const result=await carcollection.find({}).toArray()
-    res.send(result)
-  })
 
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await ordercollection.insertOne(order);
+      res.send(result);
+    });
 
-  app.post("/order", async(req,res)=>{
-    const order=req.body
-    const result=await ordercollection.insertOne(order)
-    res.send(result)
-  })
-  
-
-   app.get("/order", async(req,res)=>{
-    const result=await ordercollection.find({}).toArray()
-    res.send(result)
-
-   })
-
-
-
-
-
-
-    
+    app.get("/order", async (req, res) => {
+      const result = await ordercollection.find({}).toArray();
+      res.send(result);
+    });
   } finally {
-  
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.listen(port,(req,res)=>{
-    console.log('server runnint ', port)
-})
+app.listen(port, (req, res) => {
+  console.log("server runnint ", port);
+});
